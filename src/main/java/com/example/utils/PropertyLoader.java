ce4499a70dd6d8810330df57f4a6d0458d50aa13
Dulaj Atapattu <data1435@corp.sysco.com>
2019-11-16 15:48:06 +0530

    Add property loader

---
+++ src/main/java/com/example/utils/PropertyLoader.java
@@ -0,0 +1,31 @@
+package com.example.utils;
+
+import com.example.Main;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
+public class PropertyLoader {
+
+    public static Properties load(String propertyFileName) {
+        Properties properties = new Properties();
+
+        try (InputStream input = Main.class.getClassLoader().getResourceAsStream(propertyFileName)) {
+
+            if (input == null) {
+                System.err.println("Error while loading properties from " + propertyFileName);
+                System.exit(1);
+            }
+
+            properties.load(input);
+
+        } catch (IOException ex) {
+            System.err.println("Error while loading properties from " + propertyFileName);
+            ex.printStackTrace();
+            System.exit(1);
+        }
+
+        return properties;
+    }
+}
